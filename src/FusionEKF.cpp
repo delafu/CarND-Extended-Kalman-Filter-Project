927fd57ab4de15b8764e5281ca876f92056377df
delafu <delafu@gmail.com>
2017-10-04 23:31:29 +0200

    compiling

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -146,8 +146,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
 	Tools tools;
-	efk_.H=tools.CalculateJacobian(ekf_.x_);
-	efk_.R_=R_radar_;
+	ekf_.H=tools.CalculateJacobian(ekf_.x_);
+	ekf_.R_=R_radar_;
   } else {
     // Laser updates
 	ekf_.H_=H_laser_;
