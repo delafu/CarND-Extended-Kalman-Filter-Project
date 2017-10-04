c6e5f959c0158347d9ef1f553b28479c58abff83
delafu <delafu@gmail.com>
2017-10-04 23:47:03 +0200

    testing

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -148,12 +148,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 	Tools tools;
 	ekf_.H_=tools.CalculateJacobian(ekf_.x_);
 	ekf_.R_=R_radar_;
+	ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
 	ekf_.H_=H_laser_;
 	ekf_.R_=R_laser_;
+	ekf_.Update(measurement_pack.raw_measurements_);
   }
-  ekf_.Update(measurement_pack.raw_measurements_);
+  
 
   // print the output
   cout << "x_ = " << ekf_.x_ << endl;
