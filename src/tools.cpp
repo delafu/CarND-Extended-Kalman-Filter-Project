df90cc7e25a7e45114fcab676616aa0ef3ef0242
delafu <delafu@gmail.com>
2017-10-05 08:21:31 +0200

    testing

--- src/tools.cpp
+++ src/tools.cpp
@@ -62,15 +62,15 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 
 	//pre-compute a set of terms to avoid repeated calculation
 	float c1 = px*px+py*py;
-	float c2 = sqrt(c1);
-	float c3 = (c1*c2);
-
-	//check division by zero
 	if(c1 < .00001) {
 		px += .001;
 		py += .001;
 		c1 = px * px + py * py;
 	}
+	float c2 = sqrt(c1);
+	float c3 = (c1*c2);
+
+	//check division by zero
 
 	//compute the Jacobian matrix
 	Hj << (px/c2), (py/c2), 0, 0,
