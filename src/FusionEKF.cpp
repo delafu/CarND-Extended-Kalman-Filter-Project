f1f13f0b7724a7bafbd0a44b47ad7b5a4c0b20ce
delafu <delafu@gmail.com>
2017-10-05 08:05:47 +0200

    testing

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -21,26 +21,25 @@ FusionEKF::FusionEKF() {
 
 	Hj_ = MatrixXd(3, 4);
 
-  //measurement covariance matrix - laser
 	R_laser_ = MatrixXd(2, 2);
+	R_radar_ = MatrixXd(3, 3);
+	H_laser_ = MatrixXd(2, 4);  
+	ekf_.F_ = MatrixXd(4, 4);
+	ekf_.P_ = MatrixXd(4, 4);
+
 	R_laser_ << 0.0225, 0,
         0, 0.0225;
 
-  //measurement covariance matrix - radar
-	R_radar_ = MatrixXd(3, 3);
 	R_radar_ << 0.09, 0, 0,
         0, 0.0009, 0,
         0, 0, 0.09;
 
-	H_laser_ = MatrixXd(2, 4);  
 	H_laser_ << 1, 0, 0, 0,
 			  0, 1, 0, 0;
-	ekf_.F_ = MatrixXd(4, 4);
 	ekf_.F_ << 1, 0, 1, 0,
 			  0, 1, 0, 1,
 			  0, 0, 1, 0,
 			  0, 0, 0, 1;
-	ekf_.P_ = MatrixXd(4, 4);
 	ekf_.P_ << 1, 0, 0, 0,
 			  0, 1, 0, 0,
 			  0, 0, 1000, 0,
@@ -158,6 +157,6 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   
 
   // print the output
-  cout << "x_ = " << ekf_.x_ << endl;
-  cout << "P_ = " << ekf_.P_ << endl;
+  //cout << "x_ = " << ekf_.x_ << endl;
+  //cout << "P_ = " << ekf_.P_ << endl;
 }
